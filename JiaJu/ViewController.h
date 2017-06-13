bd3015261afe7756173777002229e8d4a955b37e
masong <iitvip@163.com>
2017-06-13 15:17:45 +0800

    Initial Commit

---
+++ JiaJu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JiaJu
+//
+//  Created by MaSong on 2017/6/13.
+//  Copyright © 2017年 MaSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JiaJu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JiaJu
+//
+//  Created by MaSong on 2017/6/13.
+//  Copyright © 2017年 MaSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
